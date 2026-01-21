62d466c432c547f39b358f5f6794f840dd7df07a
anescdev <anescdev@gmail.com>
2026-01-21 10:37:47 +0100

    Added animations Added step indicator

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,7 +2,24 @@ import type { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./button.module.css";
 import type { ButtonStyle } from "../../@types/button-style";
+import { motion } from "motion/react"
 
 export default function Button({ onClick, icon, label, buttonStyle = "primary" }: { onClick?: () => void, icon?: IconDefinition, label?: string, buttonStyle?: ButtonStyle }) {
-    return <button onClick={onClick} className={`${styles.button} ${styles[buttonStyle]}`}>{icon && <FontAwesomeIcon icon={icon} />}{label}</button>
+    return (
+        <motion.button
+            onClick={onClick}
+            className={`${styles.button} ${styles[buttonStyle]}`}
+            whileTap={{ scale: 0.98 }}
+            transition={
+                { 
+                    type: "spring",
+                    visualDuration: 0.15,
+                    bounce: 0.25
+                    
+                }
+            }
+        >
+            {icon && <FontAwesomeIcon icon={icon} />}{label}
+        </motion.button>
+    )
 }
--- src/components/step-viewer/step-viewer.tsx
+++ src/components/step-viewer/step-viewer.tsx
@@ -1,52 +1,69 @@
-import { use, useEffect, useState } from "react"
+import { use, useEffect, useRef, useState } from "react"
 import { StringArtStepsContext } from "../../contexts"
 import styles from "./step-viewer.module.css"
 import { saveStep } from "./helper";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import type { StringArtStep } from "../../@types/string-art-step";
+import { AnimatePresence, motion, useMotionValue } from 'motion/react'
 
 export default function StepViewer({ step = 0, stepsData }: { step?: number, stepsData?: StringArtStep[] }) {
     const steps = stepsData ?? use(StringArtStepsContext);
     const [currentStep, setCurrentStep] = useState(step);
-    useEffect(() => {
-        saveStep(currentStep);
-    }, [currentStep]);
+    const direction = useMotionValue<1 | -1>(1);
+    const animating = useRef(false)
     const previousButton = () => {
+        if (animating.current) return;
         setCurrentStep(currentStep => Math.max(currentStep - 1, 0));
+        direction.jump(-1, true)
     }
     const nextButton = () => {
+        if (animating.current) return;
         setCurrentStep(currentStep => Math.min(currentStep + 1, steps.length - 1));
+        direction.jump(1, true)
     }
     useEffect(() => {
         const keyboardHandler = (e: KeyboardEvent) => {
-            if (e.key === "ArrowLeft") {
+            if (e.key === "ArrowLeft" && currentStep > 0) {
                 previousButton();
-            } else if (e.key === "ArrowRight") {
+                return;
+            }
+            if (e.key === "ArrowRight" && currentStep < steps.length - 1) {
                 nextButton();
+                return;
             }
         }
         window.addEventListener("keydown", keyboardHandler);
         return () => {
             window.removeEventListener("keydown", keyboardHandler);
         }
-        
-    }, []);
+
+    }, [currentStep]);
+    useEffect(() => {
+        saveStep(currentStep);
+    }, [currentStep]);
 
     return (
         <div className={styles.viewer}>
-            <div className={styles.stepCounter}>
+            <motion.div initial={{ y: -40 }} animate={{ y: 0 }} transition={{ type: "spring", bounce: 0.30, duration: 0.25 }} className={styles.stepCounter}>
                 <span>Step Nº</span>
-                <span>{currentStep + 1}</span>
-            </div>
+                <motion.span key={currentStep} initial={{ scale: 0.6 }} animate={{ scale: 1 }} layout>{currentStep + 1}</motion.span>
+            </motion.div>
             <div className={styles.stepControls}>
-                <button disabled={currentStep === 0} onClick={previousButton}><FontAwesomeIcon icon={faAngleLeft}/></button>
-                <div className={styles.step}>
-                    <span>{steps[currentStep].from + 1}</span>
-                    <span>→</span>
-                    <span>{steps[currentStep].to + 1}</span>
-                </div>
-                <button disabled={currentStep === steps.length - 1} onClick={nextButton}><FontAwesomeIcon icon={faAngleRight}/></button>
+                <button disabled={currentStep <= 0} onClick={previousButton}><FontAwesomeIcon icon={faAngleLeft} /></button>
+                <AnimatePresence initial={false} mode="popLayout" >
+                    <motion.div key={currentStep} className={styles.step}
+                        initial={{ opacity: 1, x: direction.get() * 300 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: direction.get() * -65 }}
+                        transition={{ type: "spring", bounce: 0.25, visualDuration: 0.3 }}
+                        onAnimationStart={() => animating.current = true} onAnimationComplete={() => animating.current = false}>
+                        <motion.span className="from">{steps[currentStep].from + 1}</motion.span>
+                        <motion.span>→</motion.span>
+                        <motion.span className="to">{steps[currentStep].to + 1}</motion.span>
+                    </motion.div>
+                </AnimatePresence>
+                <button disabled={currentStep >= steps.length - 1} onClick={nextButton}><FontAwesomeIcon icon={faAngleRight} /></button>
             </div>
         </div>
     )
